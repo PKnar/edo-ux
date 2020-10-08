22fb93dd466e44de07a476db0da8ac698af9098c
Poghosyan,Knarik K <k.poghosyan@student.fontys.nl>
2020-10-08 04:15:07 +0200

    Updates

---
+++ precache-manifest.a23cae33833a7fa9d7574d0608848082.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5975eff3742d311d9327802f74cf137",
+    "url": "/edo-ux/index.html"
+  },
+  {
+    "revision": "7c14c29918cc0781bae9",
+    "url": "/edo-ux/static/css/main.c6e7d3d1.chunk.css"
+  },
+  {
+    "revision": "f46e3c5dfa4f1677fda7",
+    "url": "/edo-ux/static/js/2.12094ec5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/edo-ux/static/js/2.12094ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c14c29918cc0781bae9",
+    "url": "/edo-ux/static/js/main.fe1fb8f5.chunk.js"
+  },
+  {
+    "revision": "abbfe2eb7a8f922dd066",
+    "url": "/edo-ux/static/js/runtime-main.6ed07316.js"
+  },
+  {
+    "revision": "ff6cb81855ea776b218f2d97ad5c16ec",
+    "url": "/edo-ux/static/media/1.ff6cb818.png"
+  },
+  {
+    "revision": "faff047a098e23cbf3a984980ab86907",
+    "url": "/edo-ux/static/media/2.faff047a.png"
+  },
+  {
+    "revision": "7eeefb54e76c87f179bf91a1a99ccaf5",
+    "url": "/edo-ux/static/media/3.7eeefb54.png"
+  },
+  {
+    "revision": "832b6a7271f232fbbfddd73e12c56e23",
+    "url": "/edo-ux/static/media/4.832b6a72.png"
+  },
+  {
+    "revision": "0ba7ff7c36e80295b604d7dd9aef3b55",
+    "url": "/edo-ux/static/media/5.0ba7ff7c.png"
+  },
+  {
+    "revision": "541b2995fc5f09f6bcc975d091d2df02",
+    "url": "/edo-ux/static/media/6.541b2995.png"
+  },
+  {
+    "revision": "7ea96020a4be7e08d50d346d79cc1691",
+    "url": "/edo-ux/static/media/7.7ea96020.png"
+  },
+  {
+    "revision": "6ca723f8930030a76b49eb3ee375149d",
+    "url": "/edo-ux/static/media/Alex.6ca723f8.jpg"
+  },
+  {
+    "revision": "d48d9de0e96cf390304cf7ad5a6968ed",
+    "url": "/edo-ux/static/media/EDO-assignment.d48d9de0.pdf"
+  },
+  {
+    "revision": "82718d34fb46d379dccd4e64c668ebe8",
+    "url": "/edo-ux/static/media/Jill.82718d34.jpg"
+  },
+  {
+    "revision": "28a2075d4129f84a5aab8be9aa8979d7",
+    "url": "/edo-ux/static/media/food.28a2075d.jpg"
+  },
+  {
+    "revision": "69a3b3f8d0844bfe7222f315e64994c3",
+    "url": "/edo-ux/static/media/pexels-mohamed-abdelgaffar-771742.69a3b3f8.jpg"
+  }
+]);
